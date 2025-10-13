5bb235e44ce4650b3da233a83219426a7334c38b
Ahmad Farrel Ash Shiddiqi <farbil232@gmail.com>
2025-10-13 22:02:08 +0700

    add input method features

--- src/Sem1/method/BelajarMethodKalkulator.java
+++ src/Sem1/method/BelajarMethodKalkulator.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class BelajarMethodKalkulator {
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args){
         int angka1=5, angka2 =10, c1, c2, c3;
         double c4;
@@ -37,6 +40,25 @@ public class BelajarMethodKalkulator {
         int z=mengurangkan(menjumlahkan(10, 20), menjumlahkan(2,4));
         System.out.println("Hasil z: "+z);
 
+        // input method penjumlahan dua angka
+        System.out.println();
+        c1=inputpenjumlahan();
+        System.out.println("Hasil penjumlahan (input): "+c1);
+
+        // input method pengurangan dua angka
+        System.out.println();
+        c1=inputpengurangan();
+        System.out.println("Hasil pengurangan (input): "+c1);
+
+        // input method perkalian dua angka
+        System.out.println();
+        c1=inputperkalian();
+        System.out.println("Hasil perkalian (input): "+c1);
+        
+        // input method pembagian dua angka
+        System.out.println();
+        c4=inputpembagian();
+        System.out.println("Hasil pembagian (input): "+c4);
     }
     static int menjumlahkan(int a, int b){ // variabel 'menjumlahkan' adalah identifier | tanda '()' adalah parameter
         int c;
@@ -60,6 +82,47 @@ public class BelajarMethodKalkulator {
     }
     static void cetak(){
         System.out.println("Ini adalah program untuk kalkulator");
+    }
+
+    // input method penjumlahan
+    static int inputpenjumlahan(){
+        System.out.println("Input penjumlahan");
+        System.out.print("Masukkan angka pertama: ");
+        int a = input.nextInt();
+        System.out.print("Masukkan angka kedua: ");
+        int b = input.nextInt();
+        int hasilpenjumlahan = a + b;
+        return hasilpenjumlahan;
+    }
+    // input method pengurangan
+    static int inputpengurangan(){
+        System.out.println("Input pengurangan");
+        System.out.print("Masukkan angka pertama: ");
+        int a = input.nextInt();
+        System.out.print("Masukkan angka kedua: ");
+        int b = input.nextInt();
+        int hasilpengurangan = a - b;
+        return hasilpengurangan;
+    }
+    // input method perkalian
+    static int inputperkalian(){
+        System.out.println("Input perkalian");
+        System.out.print("Masukkan angka pertama: ");
+        int a = input.nextInt();
+        System.out.print("Masukkan angka kedua: ");
+        int b = input.nextInt();
+        int hasilperkalian = a * b;
+        return hasilperkalian;
+    }
+    // input method pembagian
+    static double inputpembagian(){
+        System.out.println("Input pembagian");
+        System.out.print("Masukkan angka pertama: ");
+        double a = input.nextDouble();
+        System.out.print("Masukkan angka kedua: ");
+        double b = input.nextDouble();
+        double hasilpembagian = a / b;
+        return hasilpembagian;
     }
 }
 
